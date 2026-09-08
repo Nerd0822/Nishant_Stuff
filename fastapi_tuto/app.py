4b9924dd042abd16fd15fea4ae0ba6198adc3513
Nerd0822 <nishantkagra420@gmail.com>
2026-09-09 01:50:33 +0530

    new: learned basic of fastapi

---
+++ fastapi_tuto/app.py
@@ -0,0 +1,51 @@
+from contextlib import asynccontextmanager
+
+from db import Post, create_db_and_tables, get_async_session
+from fastapi import Depends, FastAPI, File, Form, HTTPException, UploadFile
+from schemas import PostCreate, PostResponse
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_and_tables()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.post("/upload")
+async def upload_file(
+    file: UploadFile = File(...),
+    caption: str = Form(...),
+    session: AsyncSession = Depends(get_async_session),
+):
+    post = Post(
+        caption=caption, url="dummyurl", file_type="photo", file_name="dummy name"
+    )
+    session.add(post)
+    await session.commit()
+    await session.refresh(post)
+    return post
+
+
+@app.get("/feed")
+async def get_feed(session: AsyncSession = Depends(get_async_session)):
+    result = await session.execute(select(Post).order_by(Post.created_at.desc()))
+    posts = [row[0] for row in result.all()]
+
+    posts_data = []
+    for post in posts:
+        posts_data.append(
+            {
+                "id": str(post.id),
+                "caption": post.caption,
+                "url": post.url,
+                "file_type": post.file_type,
+                "file_name": post.file_name,
+                "created_at": post.created_at.isoformat(),
+            }
+        )
+    return {"posts": posts_data}
